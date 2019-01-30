0f5cc6b0368e6ae8c72ee10291477e4918032c89
Felipe <felipesodre20@gmail.com>
2019-01-30 20:51:37 -0200

    First

---
+++ src/main/java/com/philsodre/UdemyCourseApplication.java
@@ -0,0 +1,14 @@
+package com.philsodre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCourseApplication.class, args);
+	}
+
+}
+

3b5d0235a9a74a6d421c64076ea9368f55836f8e
Felipe <felipesodre20@gmail.com>
2019-01-30 22:46:41 -0200

    Creating Repository and Service to Categorys

--- src/main/java/com/philsodre/resources/CategoriaResource.java
+++ src/main/java/com/philsodre/resources/CategoriaResource.java
@@ -1,15 +1,15 @@
 package com.philsodre.resources;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.philsodre.domain.Categoria;
+import com.philsodre.services.CategoriaService;
 
 
 
@@ -18,15 +18,12 @@ import com.philsodre.domain.Categoria;
 public class CategoriaResource {
 	
 	@Autowired
-	//private CategoriaService service;
+	private CategoriaService service;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> find() {
-		//Categoria obj = service.find(id);
-		Categoria obj = new Categoria(1, "Tires");
-		List<Categoria> list = new ArrayList<>();
-		list.add(obj);
-		return list;
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok(obj);
 	}
 	
 }
---
+++ src/main/java/com/philsodre/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.philsodre.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.philsodre.domain.Categoria;
+import com.philsodre.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);  
+		return obj.orElse(null); 
+	}
+	
+}
